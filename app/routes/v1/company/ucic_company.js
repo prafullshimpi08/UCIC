432e392a058fa2b25edd3eb5837ba11bc112b61a
prafull <prafullshimpi2829@.com>
2026-02-05 00:33:26 +0530

    changes

--- app/routes/v1/company/ucic_company.js
+++ app/routes/v1/company/ucic_company.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const ucicCompany = require('../../../controllers/ucic_company');
 
 router.get('/listUcics', ucicCompany.listUcics);
+router.get('/review', ucicCompany.getReviews);
 
 
 module.exports = router;
